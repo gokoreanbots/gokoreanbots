5e3a95484b8dda86ee42468fa4a8b143d1088377
simsimler <72448769+simsimler@users.noreply.github.com>
2021-02-16 22:31:48 +0900

    Just test

--- gokoreanbots_test.go
+++ gokoreanbots_test.go
@@ -10,14 +10,33 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+var (
+	kbclient = NewClient(session, "", true)
+	session, _ = discordgo.New("Bot ")
+)
 
 func TestRun(t *testing.T) {
-	session, _ := discordgo.New("Bot DiscordBotToken")
-	client := NewClient(session, "KoreanbotsToken", true)
 	session.Open()
 	fmt.Println(session.State.User.Username + "로 로그인했습니다.")
-	fmt.Println(client.IsVoted(710687896809766952))
+	session.AddHandler(messageCreate)
 	sc := make(chan os.Signal)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM)
 	<-sc
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if s.State.User.ID == m.Author.ID {
+		return
+	}
+	switch m.Content {
+	case "!votecheck":
+		fmt.Println("user used votecheck command")
+		isVoted := kbclient.IsVoted(m.Author.ID)
+		fmt.Println(isVoted)
+		if isVoted {
+			s.ChannelMessageSend(m.ChannelID, "하트를 누르셨군요")
+		} else {
+			s.ChannelMessageSend(m.ChannelID, "하트를 누르지 않으셨군요")
+		}
+	}
 }
