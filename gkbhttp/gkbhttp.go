c3fdd49e0d071b008e5edb074c97297ad8023c0f
simsimler <72448769+simsimler@users.noreply.github.com>
2021-02-16 14:21:05 +0900

    Add Post and Get function

---
+++ gkbhttp/gkbhttp.go
@@ -0,0 +1,37 @@
+package gkbhttp
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/simsimler/gokoreanbots/gkberrors"
+)
+
+// Post somthing
+func Post(url string, headers map[string]string, jsonData []byte) error{
+	client := http.Client{}
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return gkberrors.ErrFailToPost
+	}
+	defer res.Body.Close()
+	return nil
+}
+
+// Get something
+func Get(url string, headers map[string]string) string {
+	client := http.Client{}
+	req, _ := http.NewRequest("GET", url, nil)
+	for key, value := range headers {
+		req.Header.Add(key, value)
+	}
+	res, _ := client.Do(req)
+	responseByte, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	return string(responseByte)
+}
